4ef3f1a5b03ad0b0ea37db671ecd95c0acc52cc2
zhulduzz <123058905+zhulduzz@users.noreply.github.com>
2024-02-20 21:54:38 +0600

    Initial commit

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+
+  imports: [],
+  templateUrl: './product-alerts.component.html',
+  styleUrl: './product-alerts.component.css',
+})
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+}
